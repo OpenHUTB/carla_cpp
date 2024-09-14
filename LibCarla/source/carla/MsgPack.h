22af4034d883aec4531f2b905f6396108180ee60
smile <1359235070@qq.com>
2024-09-14 19:26:27 +0800

    update

--- LibCarla/source/carla/MsgPack.h
+++ LibCarla/source/carla/MsgPack.h
@@ -4,36 +4,47 @@
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
 
-#pragma once
+#pragma once  // 确保该头文件仅被包含一次
 
-#include "carla/Buffer.h"
+#include "carla/Buffer.h"  // 包含 Buffer 类的定义
 
-#include <rpc/msgpack.hpp>
+#include <rpc/msgpack.hpp>  // 包含 MessagePack 的头文件
 
-namespace carla {
+namespace carla {  // carla 命名空间
 
-  class MsgPack {
+  class MsgPack {  // MsgPack 类定义
   public:
 
+    // 模板方法，序列化对象 obj 为 Buffer 类型
     template <typename T>
     static Buffer Pack(const T &obj) {
-      namespace mp = ::clmdep_msgpack;
-      mp::sbuffer sbuf;
-      mp::pack(sbuf, obj);
+      namespace mp = ::clmdep_msgpack;  // 为 MessagePack 命名空间起别名
+
+      mp::sbuffer sbuf;  // 创建一个 MessagePack 的缓冲区对象
+      mp::pack(sbuf, obj);  // 使用 MessagePack 序列化对象 obj 到 sbuf 中
+
+      // 将 sbuf 中的数据和大小封装到 Buffer 对象中并返回
       return Buffer(reinterpret_cast<const unsigned char *>(sbuf.data()), sbuf.size());
     }
 
+    // 模板方法，从 Buffer 对象反序列化为对象 T
     template <typename T>
     static T UnPack(const Buffer &buffer) {
-      namespace mp = ::clmdep_msgpack;
+      namespace mp = ::clmdep_msgpack;  // 为 MessagePack 命名空间起别名
+
+      // 使用 MessagePack 从 Buffer 中的数据反序列化为对象 T
       return mp::unpack(reinterpret_cast<const char *>(buffer.data()), buffer.size()).template as<T>();
     }
 
+    // 模板方法，从原始数据（unsigned char *）和大小反序列化为对象 T
     template <typename T>
     static T UnPack(const unsigned char *data, size_t size) {
-      namespace mp = ::clmdep_msgpack;
+      namespace mp = ::clmdep_msgpack;  // 为 MessagePack 命名空间起别名
+
+      // 使用 MessagePack 从原始数据反序列化为对象 T
       return mp::unpack(reinterpret_cast<const char *>(data), size).template as<T>();
     }
   };
 
 } // namespace carla
+
