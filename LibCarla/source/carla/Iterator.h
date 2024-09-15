baac39f329203e88d2a178ef4d23cbfbbff317ee
minghao-lee <1467864011@qq.com>
2024-09-15 14:21:36 +0800

    添加注释

--- LibCarla/source/carla/Iterator.h
+++ LibCarla/source/carla/Iterator.h
@@ -11,7 +11,7 @@
 namespace carla {
 namespace iterator {
 
- 
+ /// Creates an iterator over non-const references to the keys of a map.
   template <typename It>
   inline static auto make_map_keys_iterator(It it) {
   	// 确定输入迭代器指向的键值对中键的类型
@@ -23,8 +23,8 @@ namespace iterator {
      // 使用 Boost 的 transform_iterator 创建一个新的迭代器，该迭代器在解引用时会调用 lambda 表达式，返回对键的非 const 引用
     return boost::make_transform_iterator(it, [](auto &pair) -> ref_to_first { return pair.first; });
   }
-
-  
+  /// Creates an iterator over const references to the keys of a map.
+  template <typename It>
   inline static auto make_map_keys_const_iterator(It it) {
     using first_value_type = typename It::value_type::first_type;
     using decay_first_value_type = typename std::remove_cv_t<typename std::remove_reference_t<first_value_type>>;
@@ -32,7 +32,7 @@ namespace iterator {
     return boost::make_transform_iterator(it, [](const auto &pair) -> const_ref_to_first { return pair.first; });
   }
 
-  
+  /// Creates an iterator over non-const references to the values of a map.
   template <typename It>
   inline static auto make_map_values_iterator(It it) {
     using second_value_type = typename It::value_type::second_type;
@@ -42,7 +42,7 @@ namespace iterator {
     return boost::make_transform_iterator(it, [](auto &pair) -> ref_to_second { return pair.second; });
   }
 
- 
+ /// Creates an iterator over const references to the values of a map.
   template <typename It>
   inline static auto make_map_values_const_iterator(It it) {
     using second_value_type = typename It::value_type::second_type;
