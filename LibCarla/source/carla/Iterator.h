c59fc8d1226968d8e1a3e0418eb95085b6048982
minghao-lee <1467864011@qq.com>
2024-09-15 13:45:07 +0800

    添加注释

--- LibCarla/source/carla/Iterator.h
+++ LibCarla/source/carla/Iterator.h
@@ -11,7 +11,7 @@
 namespace carla {
 namespace iterator {
 
-  // 创建一个迭代器，用于遍历非 const 引用的地图键
+ 
   template <typename It>
   inline static auto make_map_keys_iterator(It it) {
   	// 确定输入迭代器指向的键值对中键的类型
@@ -24,8 +24,7 @@ namespace iterator {
     return boost::make_transform_iterator(it, [](auto &pair) -> ref_to_first { return pair.first; });
   }
 
-  // 创建一个迭代器，用于遍历 const 引用的地图键
-  template <typename It>
+  
   inline static auto make_map_keys_const_iterator(It it) {
     using first_value_type = typename It::value_type::first_type;
     using decay_first_value_type = typename std::remove_cv_t<typename std::remove_reference_t<first_value_type>>;
@@ -33,21 +32,23 @@ namespace iterator {
     return boost::make_transform_iterator(it, [](const auto &pair) -> const_ref_to_first { return pair.first; });
   }
 
-  // 创建一个迭代器，用于遍历非 const 引用的地图值
+  
   template <typename It>
   inline static auto make_map_values_iterator(It it) {
     using second_value_type = typename It::value_type::second_type;
     using decay_second_value_type = typename std::remove_cv_t<typename std::remove_reference_t<second_value_type>>;
     using ref_to_second = decay_second_value_type &;
+    // 使用 Boost 的 transform_iterator 创建一个新的迭代器，该迭代器在解引用时会调用 lambda 表达式，返回对值的非 const 引用
     return boost::make_transform_iterator(it, [](auto &pair) -> ref_to_second { return pair.second; });
   }
 
-  // 创建一个迭代器，用于遍历 const 引用的地图值
+ 
   template <typename It>
   inline static auto make_map_values_const_iterator(It it) {
     using second_value_type = typename It::value_type::second_type;
     using decay_second_value_type = typename std::remove_cv_t<typename std::remove_reference_t<second_value_type>>;
     using const_ref_to_second = const decay_second_value_type &;
+    // 使用 Boost 的 transform_iterator 创建一个新的迭代器，该迭代器在解引用时会调用 lambda 表达式，返回对值的 const 引用
     return boost::make_transform_iterator(it, [](const auto &pair) -> const_ref_to_second { return pair.second; });
   }
 
