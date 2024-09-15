2b499e93ac84e0094f902bb2b93358e9ad2c64cc
smile <1359235070@qq.com>
2024-09-15 11:22:13 +0800

    update

--- LibCarla/source/carla/Buffer.cpp
+++ LibCarla/source/carla/Buffer.cpp
@@ -1,14 +1,16 @@
-#include "carla/Buffer.h"
+#include "carla/Buffer.h"  // 包含 Buffer 类的定义
 
-#include "carla/BufferPool.h"
+#include "carla/BufferPool.h"  // 包含 BufferPool 类的定义
 
-namespace carla {
+namespace carla {  // carla 命名空间，封装相关类和函数
 
+  // Buffer 类的成员函数，实现重新利用当前缓冲区
   void Buffer::ReuseThisBuffer() {
-    auto pool = _parent_pool.lock();
-    if (pool != nullptr) {
-      pool->Push(std::move(*this));
+    auto pool = _parent_pool.lock();  // 尝试锁定并获取对父池的弱引用
+    if (pool != nullptr) {  // 如果成功获取到父池
+      pool->Push(std::move(*this));  // 将当前缓冲区移动到父池中
     }
   }
 
-} //名称空间 
+} // namespace carla
+
