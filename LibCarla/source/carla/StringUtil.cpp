1488fb3d6071347c879e7c7b9a9d6a37337b8b26
06 <2753462813@qq.com>
2024-09-18 00:28:12 +0800

    update

--- LibCarla/source/carla/StringUtil.cpp
+++ LibCarla/source/carla/StringUtil.cpp
@@ -12,10 +12,10 @@
 #  include <fnmatch.h>
 #endif // _WIN32
 
-namespace carla {
+namespace carla {  // 检查字符串是否与模式匹配的函数
 
   bool StringUtil::Match(const char *str, const char *test) {
-#ifdef _WIN32
+#ifdef _WIN32      // 在 Windows 平台上，使用 PathMatchSpecA 函数进行匹配
     return PathMatchSpecA(str, test);
 #else
     return 0 == fnmatch(test, str, 0);
