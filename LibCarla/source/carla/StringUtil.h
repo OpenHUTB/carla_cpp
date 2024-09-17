d5ec1673964ee5155f5c317d1b04c6968a05ffa1
minghao-lee <1467864011@qq.com>
2024-09-17 14:26:53 +0800

    添加注释

--- LibCarla/source/carla/StringUtil.h
+++ LibCarla/source/carla/StringUtil.h
@@ -9,21 +9,22 @@
 #include <boost/algorithm/string.hpp>
 
 namespace carla {
-
+// 定义名为 StringUtil 的类，用于提供各种字符串处理工具方法
   class StringUtil {
   public:
-
+// 将 const char* 类型的字符串转换为 const char*，直接返回输入参数
     static const char *ToConstCharPtr(const char *str) {
       return str;
     }
-
+ // 对于模板类型的字符串，将其转换为 const char*，通过调用其 c_str() 方法
     template <typename StringT>
     static const char *ToConstCharPtr(const StringT &str) {
       return str.c_str();
     }
-
+// 判断输入范围 input 是否以范围 test 开头
     template <typename Range1T, typename Range2T>
     static bool StartsWith(const Range1T &input, const Range2T &test) {
+    	// 使用 boost::algorithm::istarts_with 函数进行判断
       return boost::algorithm::istarts_with(input, test);
     }
 
@@ -36,32 +37,32 @@ namespace carla {
     static void ToLower(WritableRangeT &str) {
       boost::algorithm::to_lower(str);
     }
-
+// 将可写范围的字符串转换为小写形式
     template <typename SequenceT>
     static auto ToLowerCopy(const SequenceT &str) {
       return boost::algorithm::to_lower_copy(str);
     }
-
+// 将可写范围的字符串转换为大写形式
     template <typename WritableRangeT>
     static void ToUpper(WritableRangeT &str) {
       boost::algorithm::to_upper(str);
     }
-
+ 
     template <typename SequenceT>
     static auto ToUpperCopy(const SequenceT &str) {
       return boost::algorithm::to_upper_copy(str);
     }
-
+ // 去除可写范围字符串两端的空白字符
     template <typename WritableRangeT>
     static void Trim(WritableRangeT &str) {
       boost::algorithm::trim(str);
     }
-
+// 对于不可写范围的字符串，返回去除两端空白字符后的副本
     template <typename SequenceT>
     static auto TrimCopy(const SequenceT &str) {
       return boost::algorithm::trim_copy(str);
     }
-
+// 将字符串 str 按照分隔符集合 separators 进行分割，结果存储在 destination 容器中
     template<typename Container, typename Range1T, typename Range2T>
     static void Split(Container &destination, const Range1T &str, const Range2T &separators) {
       boost::split(destination, str, boost::is_any_of(separators));
