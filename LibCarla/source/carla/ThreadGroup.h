c7c1e279a7f0f5a191a23f2ed701926ea598f1dc
smile <1359235070@qq.com>
2024-09-17 22:36:32 +0800

    update

--- LibCarla/source/carla/ThreadGroup.h
+++ LibCarla/source/carla/ThreadGroup.h
@@ -29,14 +29,14 @@ namespace carla {
 
     template <typename F>
     void CreateThread(F &&functor) {
-      _threads.emplace_back(std::forward<F>(functor));
+      _threads.emplace_back(std::forward<F>(functor)); // 将线程添加到容器中
     }
-
+    // 创建多个线程，每个线程执行相同的可调用对象
     template <typename F>
     void CreateThreads(size_t count, F functor) {
-      _threads.reserve(_threads.size() + count);
+      _threads.reserve(_threads.size() + count); // 预留足够的空间以避免频繁的内存分配
       for (size_t i = 0u; i < count; ++i) {
-        CreateThread(functor);
+        CreateThread(functor); // 创建线程并执行 functor
       }
     }
 
