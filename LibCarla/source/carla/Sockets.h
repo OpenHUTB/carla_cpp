80503abb46f18b59968ef7456f4b68dd747e0014
bbbelieve <2391273135@qq.com>
2024-09-18 12:43:11 +0800

    update

--- LibCarla/source/carla/Sockets.h
+++ LibCarla/source/carla/Sockets.h
@@ -8,9 +8,9 @@
 
 #if _WIN32   // 判断是否是在Windows平台编译
   #include <winsock2.h>    // 引入Windows Sockets 2 API，用于网络通信  
-  #include <Ws2tcpip.h>
-#else
-  #include <sys/socket.h> ///< socket
+  #include <Ws2tcpip.h>   // 包含TCP/IP协议的函数和数据结构定义 
+#else   // 如果不是Windows平台，则假设是类Unix系统（如Linux、macOS） 
+  #include <sys/socket.h> // 包含socket API的定义，用于网络通信
   #include <netinet/in.h> ///< sockaddr_in
   #include <arpa/inet.h>  ///< getsockname
   #include <unistd.h>     ///< close
