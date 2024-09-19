bb452930c0fa327e6188bde305121f24de308ba2
smile947 <1359235070@qq.com>
2024-09-19 16:31:26 +0800

    添加注释 (#362)
    * update
    
    * update
    
    * update
    
    * update

--- LibCarla/source/carla/MoveHandler.h
+++ LibCarla/source/carla/MoveHandler.h
@@ -4,13 +4,13 @@
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
 
-#pragma once
+#pragma once  // 确保此头文件仅被包含一次
 
-#include <type_traits>
-#include <utility>
+#include <type_traits>  // 包含类型特征相关的头文件，提供类型特性支持
+#include <utility>  // 包含通用工具函数，比如 std::move
 
-namespace carla {
-namespace detail {
+namespace carla {  // 定义命名空间 carla
+namespace detail {   // 定义命名空间 detail，用于实现细节
 
   template <typename FunctorT>
   struct MoveWrapper : FunctorT {
