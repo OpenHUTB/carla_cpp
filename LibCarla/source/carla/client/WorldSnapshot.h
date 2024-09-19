8177846660a459a63f0669404da778687a9d6b87
minghao-lee <1467864011@qq.com>
2024-09-19 23:11:48 +0800

    添加注释

--- LibCarla/source/carla/client/WorldSnapshot.h
+++ LibCarla/source/carla/client/WorldSnapshot.h
@@ -21,7 +21,6 @@ namespace client {
     WorldSnapshot(std::shared_ptr<const detail::EpisodeState> state)
       : _state(std::move(state)) {}
 
-    /// Get the id of the episode associated with this world.
 	// 获取与这个世界相关联的剧集的 ID
     uint64_t GetId() const {
       return _state->GetEpisodeId();
@@ -31,37 +30,31 @@ namespace client {
       return GetTimestamp().frame;
     }
 
-    /// Get timestamp of this snapshot.
 	// 获取这个快照的时间戳
     const Timestamp &GetTimestamp() const {
       return _state->GetTimestamp();
     }
 
-    /// Check if an actor is present in this snapshot.
 	// 检查一个参与者是否在这个快照中存在
     bool Contains(ActorId actor_id) const {
       return _state->ContainsActorSnapshot(actor_id);
     }
 
-    /// Find an ActorSnapshot by id.
 	// 通过 ID 查找一个参与者的快照，如果找到则返回 boost::optional<ActorSnapshot>，否则返回 boost::none
     boost::optional<ActorSnapshot> Find(ActorId actor_id) const {
       return _state->GetActorSnapshotIfPresent(actor_id);
     }
 
-    /// Return number of ActorSnapshots present in this WorldSnapshot.
 	// 返回这个世界快照中参与者快照的数量
     size_t size() const {
       return _state->size();
     }
 
-    /// Return a begin iterator to the list of ActorSnapshots.
 	// 返回指向参与者快照列表的开始迭代器
     auto begin() const {
       return _state->begin();
     }
 
-    /// Return a past-the-end iterator to the list of ActorSnapshots.
 	// 返回指向参与者快照列表的结束迭代器
     auto end() const {
       return _state->end();
