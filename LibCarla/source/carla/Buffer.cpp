b34c0c70e5c931e0ba3ceb40daac6738f0b8cae3
smile <1359235070@qq.com>
2024-09-20 11:43:16 +0800

    update

--- LibCarla/source/carla/Buffer.cpp
+++ LibCarla/source/carla/Buffer.cpp
@@ -5,9 +5,9 @@
 namespace carla {
 
   void Buffer::ReuseThisBuffer() {  // 定义 Buffer 类的 ReuseThisBuffer 方法
-    auto pool = _parent_pool.lock();
-    if (pool != nullptr) {
-      pool->Push(std::move(*this));
+    auto pool = _parent_pool.lock();  // 尝试锁定指向父池的弱指针
+    if (pool != nullptr) {  // 检查池是否有效（非空）
+      pool->Push(std::move(*this));  // 将当前 Buffer 实例移动到池的 Push 方法中
     }
   }
 
