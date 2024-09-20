33cea06ecc15acf8b26e3ebc97cdafe6dda0b180
smile <1359235070@qq.com>
2024-09-21 00:02:34 +0800

    update

--- LibCarla/source/carla/Exception.cpp
+++ LibCarla/source/carla/Exception.cpp
@@ -4,19 +4,19 @@
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
 
-#include "carla/Exception.h"
-#include <boost/assert/source_location.hpp>
+#include "carla/Exception.h"  // 引入Carla异常处理头文件
+#include <boost/assert/source_location.hpp>  // 引入Boost的源位置头文件
 
 // =============================================================================
 // -- Define boost::throw_exception --------------------------------------------
 // =============================================================================
 
-#ifdef BOOST_NO_EXCEPTIONS
+#ifdef BOOST_NO_EXCEPTIONS  // 如果禁用异常
 
-namespace boost {
+namespace boost {  // 在boost命名空间中
 
-  void throw_exception(const std::exception &e) {
-    carla::throw_exception(e);
+  void throw_exception(const std::exception &e) {  // 定义抛出异常函数
+    carla::throw_exception(e);  // 调用Carla的抛出异常函数
   }
 
   void throw_exception(
