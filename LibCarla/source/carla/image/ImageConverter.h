d393cde49b38462e2d314ab2b783279143106cd3
yangh05 <3494493750@qq.com>
2024-09-26 12:43:35 +0800

    update

--- LibCarla/source/carla/image/ImageConverter.h
+++ LibCarla/source/carla/image/ImageConverter.h
@@ -4,31 +4,29 @@
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
 
-#pragma once  // ֻ    һ θ ͷ ļ   ָ 
+#pragma once // 确保头文件只被包含一次
 
-#include "carla/image/ImageView.h" //      ImageView ͷ ļ   
+#include "carla/image/ImageView.h" // 引入ImageView头文件
 
-namespace carla {  //      carla      ռ䡣
-namespace image {  //      image      ռ䡣
+namespace carla { // carla命名空间
+namespace image { // image子命名空间
 
-  class ImageConverter {  //     ͼ  ת     ࡣ
-  public:   //    з      η   
+  class ImageConverter { // 定义ImageConverter类
+  public:
 
-      // ģ 庯       ڸ      ء 
-    template <typename SrcViewT, typename DstViewT>
-    static void CopyPixels(const SrcViewT &src, DstViewT &dst) {
-      boost::gil::copy_pixels(src, dst); // ʹ   boost::gil  ⸴  Դ  ͼ     ص Ŀ    ͼ  
+    template <typename SrcViewT, typename DstViewT> // 模板函数，接受源视图和目标视图类型
+    static void CopyPixels(const SrcViewT &src, DstViewT &dst) { // 静态成员函数，复制像素
+      boost::gil::copy_pixels(src, dst); // 使用Boost.GIL库复制像素
     }
 
-     // ģ 庯       ھ͵ ת  ͼ  
-    template <typename ColorConverter, typename MutableImageView>
-    static void ConvertInPlace(
-        MutableImageView &image_view, //  ɱ ͼ    ͼ   á 
-        ColorConverter converter = ColorConverter()) { //   ɫת      Ĭ  ֵΪ ColorConverter()  
-      using DstPixelT = typename MutableImageView::value_type;  //   ȡ ɱ ͼ    ͼ         ͡ 
-      CopyPixels(  //      CopyPixels       
-          ImageView::MakeColorConvertedView<MutableImageView, DstPixelT>(image_view, converter),
-          image_view);  //   ת       ͼ   ƻ ԭʼͼ    ͼ  
+    template <typename ColorConverter, typename MutableImageView> // 模板函数，接受颜色转换器和可变图像视图类型
+    static void ConvertInPlace( // 静态成员函数，原地转换图像
+        MutableImageView &image_view, // 可变图像视图引用
+        ColorConverter converter = ColorConverter()) { // 默认颜色转换器
+      using DstPixelT = typename MutableImageView::value_type; // 获取目标像素类型
+      CopyPixels( // 调用CopyPixels函数
+          ImageView::MakeColorConvertedView<MutableImageView, DstPixelT>(image_view, converter), // 创建颜色转换后的视图
+          image_view); // 目标为原始图像视图
     }
   };
 
