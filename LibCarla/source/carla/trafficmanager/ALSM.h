a7481d517ad953bff4ea21cdef1f63175edeabf3
cm1106-ai <2753462813@qq.com>
2024-10-08 17:53:42 +0800

    update

--- LibCarla/source/carla/trafficmanager/ALSM.h
+++ LibCarla/source/carla/trafficmanager/ALSM.h
@@ -30,8 +30,8 @@ namespace chr = std::chrono;  // 引用时间相关的命名空间
 namespace cg = carla::geom;   // 引用几何相关的命名空间
 namespace cc = carla::client;  // 引用客户端相关的命名空间
 
-using ActorList = carla::SharedPtr<cc::ActorList>; // 定义演员列表共享指针类型
-using ActorMap = std::unordered_map<ActorId, ActorPtr>; // 定义演员映射表类型
+using ParticipantList = carla::SharedPtr<cc::ActorList>; // 定义参与者列表共享指针类型
+using ParticipantMap = std::unordered_map<ActorId, ActorPtr>; // 定义参与者映射表类型
 using IdleTimeMap = std::unordered_map<ActorId, double>; // 定义闲置时间映射表类型
 using LocalMapPtr = std::shared_ptr<InMemoryMap>; // 定义本地地图共享指针类型
 
@@ -41,13 +41,13 @@ using LocalMapPtr = std::shared_ptr<InMemoryMap>; // 定义本地地图共享指
 class ALSM {
 
 private:
-  AtomicActorSet &registered_vehicles; // 引用已注册车辆的原子集合
-  ActorMap unregistered_actors; // 存储未注册车辆的结构
+  AtomicActorSet &registered_vehicles; // 引用已注册参与者的原子集合
+  ParticipantMap unregistered_participants; // 存储未注册参与者的结构
   BufferMap &buffer_map; // 引用缓冲区映射
-  IdleTimeMap idle_time; // 存储车辆在位置上停留时间的结构
-  ActorMap hero_actors; // 存储角色名称为"hero"的车辆
+  IdleTimeMap idle_time; // 存储参与者在位置上停留时间的结构
+  ParticipantMap hero_participants; // 存储角色名称为"hero"的参与者
   TrackTraffic &track_traffic; // 引用交通跟踪对象
-  std::vector<ActorId>& marked_for_removal; // 标记待移除车辆的数组
+  std::vector<ActorId>& marked_for_removal; // 标记待移除参与者的数组
   const Parameters &parameters; // 引用参数对象
   const cc::World &world; // 引用世界对象
   const LocalMapPtr &local_map; // 引用本地地图指针
@@ -57,33 +57,33 @@ private:
   TrafficLightStage &traffic_light_stage; // 引用交通灯阶段对象
   MotionPlanStage &motion_plan_stage; // 引用运动规划阶段对象
   VehicleLightStage &vehicle_light_stage; // 引用车辆灯光阶段对象
-  double elapsed_last_actor_destruction {0.0}; // 记录自上次因闲置过久而销毁车辆的时间
+  double elapsed_last_participant_destruction {0.0}; // 记录自上次因闲置过久而销毁参与者的时间
   cc::Timestamp current_timestamp; // 当前时间戳
-  std::unordered_map<ActorId, bool> has_physics_enabled; // 存储每个演员是否启用物理的映射
+  std::unordered_map<ActorId, bool> has_physics_enabled; // 存储每个参与者是否启用物理的映射
 
-  // 更新已注册车辆在某位置上停留的时间
+  // 更新已注册参与者在某位置上停留的时间
   void UpdateIdleTime(std::pair<ActorId, double>& max_idle_time, const ActorId& actor_id);
 
   // 判断一辆车是否长时间停滞不前
   bool IsVehicleStuck(const ActorId& actor_id);
 
-  // 确定自上次更新以来在仿真中新生成的演员
-  void IdentifyNewActors(const ActorList &actor_list);
+  // 确定自上次更新以来在仿真中新生成的参与者
+  void IdentifyNewParticipants(const ParticipantList &participant_list);
 
-  using DestroyeddActors = std::pair<ActorIdSet, ActorIdSet>; // 定义删除演员的数据类型
-  // 确定在上一帧中删除的演员
-  // 返回已注册和未注册演员的数组
-  DestroyeddActors IdentifyDestroyedActors(const ActorList &actor_list);
+  using DestroyeddParticipants = std::pair<ActorIdSet, ActorIdSet>; // 定义删除参与者的数据类型
+  // 确定在上一帧中删除的参与者
+  // 返回已注册和未注册参与者的数组
+  DestroyeddParticipants IdentifyDestroyedParticipants(const ParticipantList &participant_list);
 
   using IdleInfo = std::pair<ActorId, double>; // 定义闲置信息的数据类型
-  void UpdateRegisteredActorsData(const bool hybrid_physics_mode, IdleInfo &max_idle_time);
+  void UpdateRegisteredParticipantsData(const bool hybrid_physics_mode, IdleInfo &max_idle_time);
 
-  // 更新车辆数据
+  // 更新参与者数据
   void UpdateData(const bool hybrid_physics_mode, const Actor &vehicle,
                   const bool hero_actor_present, const float physics_radius_square);
 
-  // 更新未注册车辆的数据
-  void UpdateUnregisteredActorsData();
+  // 更新未注册参与者的数据
+  void UpdateUnregisteredParticipantsData();
 
 public:
   // 构造函数
@@ -104,11 +104,9 @@ public:
   // 更新方法
   void Update();
 
-  // 从交通管理中移除演员，并清理与该车辆相关的各种数据
-  void RemoveActor(const ActorId actor_id, const bool registered_actor);
+  // 从交通管理中移除参与者，并清理与该车辆相关的各种数据
+  void RemoveParticipant(const ActorId actor_id, const bool registered_actor);
 
   // 重置方法
   void Reset();
 };
-} // namespace traffic_manager
-} // namespace carla
