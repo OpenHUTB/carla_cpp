24d6ada1e0210ef143a7aba548f73823c4c685a8
cm1106-ai <2753462813@qq.com>
2024-10-08 15:45:31 +0800

    update

--- LibCarla/source/carla/trafficmanager/ALSM.h
+++ LibCarla/source/carla/trafficmanager/ALSM.h
@@ -23,72 +23,70 @@
 namespace carla {
 namespace traffic_manager {
 
-using namespace constants::HybridMode;
-using namespace constants::VehicleRemoval;
+using namespace constants::HybridMode;  // 引入混合模式常量
+using namespace constants::VehicleRemoval; // 引入车辆移除常量
 
-namespace chr = std::chrono;
-namespace cg = carla::geom;
-namespace cc = carla::client;
+namespace chr = std::chrono;  // 引用时间相关的命名空间
+namespace cg = carla::geom;   // 引用几何相关的命名空间
+namespace cc = carla::client;  // 引用客户端相关的命名空间
 
-using ActorList = carla::SharedPtr<cc::ActorList>;
-using ActorMap = std::unordered_map<ActorId, ActorPtr>;
-using IdleTimeMap = std::unordered_map<ActorId, double>;
-using LocalMapPtr = std::shared_ptr<InMemoryMap>;
+using ActorList = carla::SharedPtr<cc::ActorList>; // 定义演员列表共享指针类型
+using ActorMap = std::unordered_map<ActorId, ActorPtr>; // 定义演员映射表类型
+using IdleTimeMap = std::unordered_map<ActorId, double>; // 定义闲置时间映射表类型
+using LocalMapPtr = std::shared_ptr<InMemoryMap>; // 定义本地地图共享指针类型
 
-/// ALSM: Agent Lifecycle and State Managerment
-/// This class has functionality to update the local cache of kinematic states
-/// and manage memory and cleanup for varying number of vehicles in the simulation.
+/// ALSM: 代理生命周期和状态管理
+/// 此类具有更新运动状态本地缓存的功能
+/// 并管理模拟中车辆数量变化的内存和清理。
 class ALSM {
 
 private:
-  AtomicActorSet &registered_vehicles;
-  // Structure containing vehicles in the simulator not registered with the traffic manager.
-  ActorMap unregistered_actors;
-  BufferMap &buffer_map;
-  // Structure keeping track of duration of vehicles stuck in a location.
-  IdleTimeMap idle_time;
-  // Structure containing vehicles with attribute role_name with value hero.
-  ActorMap hero_actors;
-  TrackTraffic &track_traffic;
-  // Array of vehicles marked by stages for removal.
-  std::vector<ActorId>& marked_for_removal;
-  const Parameters &parameters;
-  const cc::World &world;
-  const LocalMapPtr &local_map;
-  SimulationState &simulation_state;
-  LocalizationStage &localization_stage;
-  CollisionStage &collision_stage;
-  TrafficLightStage &traffic_light_stage;
-  MotionPlanStage &motion_plan_stage;
-  VehicleLightStage &vehicle_light_stage;
-  // Time elapsed since last vehicle destruction due to being idle for too long.
-  double elapsed_last_actor_destruction {0.0};
-  cc::Timestamp current_timestamp;
-  std::unordered_map<ActorId, bool> has_physics_enabled;
+  AtomicActorSet &registered_vehicles; // 引用已注册车辆的原子集合
+  ActorMap unregistered_actors; // 存储未注册车辆的结构
+  BufferMap &buffer_map; // 引用缓冲区映射
+  IdleTimeMap idle_time; // 存储车辆在位置上停留时间的结构
+  ActorMap hero_actors; // 存储角色名称为"hero"的车辆
+  TrackTraffic &track_traffic; // 引用交通跟踪对象
+  std::vector<ActorId>& marked_for_removal; // 标记待移除车辆的数组
+  const Parameters &parameters; // 引用参数对象
+  const cc::World &world; // 引用世界对象
+  const LocalMapPtr &local_map; // 引用本地地图指针
+  SimulationState &simulation_state; // 引用仿真状态对象
+  LocalizationStage &localization_stage; // 引用定位阶段对象
+  CollisionStage &collision_stage; // 引用碰撞阶段对象
+  TrafficLightStage &traffic_light_stage; // 引用交通灯阶段对象
+  MotionPlanStage &motion_plan_stage; // 引用运动规划阶段对象
+  VehicleLightStage &vehicle_light_stage; // 引用车辆灯光阶段对象
+  double elapsed_last_actor_destruction {0.0}; // 记录自上次因闲置过久而销毁车辆的时间
+  cc::Timestamp current_timestamp; // 当前时间戳
+  std::unordered_map<ActorId, bool> has_physics_enabled; // 存储每个演员是否启用物理的映射
 
-  // Updates the duration for which a registered vehicle is stuck at a location.
+  // 更新已注册车辆在某位置上停留的时间
   void UpdateIdleTime(std::pair<ActorId, double>& max_idle_time, const ActorId& actor_id);
 
-  // Method to determine if a vehicle is stuck at a place for too long.
+  // 判断一辆车是否长时间停滞不前
   bool IsVehicleStuck(const ActorId& actor_id);
 
-  // Method to identify actors newly spawned in the simulation since last tick.
+  // 确定自上次更新以来在仿真中新生成的演员
   void IdentifyNewActors(const ActorList &actor_list);
 
-  using DestroyeddActors = std::pair<ActorIdSet, ActorIdSet>;
-  // Method to identify actors deleted in the last frame.
-  // Arrays of registered and unregistered actors are returned separately.
+  using DestroyeddActors = std::pair<ActorIdSet, ActorIdSet>; // 定义删除演员的数据类型
+  // 确定在上一帧中删除的演员
+  // 返回已注册和未注册演员的数组
   DestroyeddActors IdentifyDestroyedActors(const ActorList &actor_list);
 
-  using IdleInfo = std::pair<ActorId, double>;
+  using IdleInfo = std::pair<ActorId, double>; // 定义闲置信息的数据类型
   void UpdateRegisteredActorsData(const bool hybrid_physics_mode, IdleInfo &max_idle_time);
 
+  // 更新车辆数据
   void UpdateData(const bool hybrid_physics_mode, const Actor &vehicle,
                   const bool hero_actor_present, const float physics_radius_square);
 
+  // 更新未注册车辆的数据
   void UpdateUnregisteredActorsData();
 
 public:
+  // 构造函数
   ALSM(AtomicActorSet &registered_vehicles,
        BufferMap &buffer_map,
        TrackTraffic &track_traffic,
@@ -103,14 +101,14 @@ public:
        MotionPlanStage &motion_plan_stage,
        VehicleLightStage &vehicle_light_stage);
 
+  // 更新方法
   void Update();
 
-  // Removes an actor from traffic manager and performs clean up of associated data
-  // from various stages tracking the said vehicle.
+  // 从交通管理中移除演员，并清理与该车辆相关的各种数据
   void RemoveActor(const ActorId actor_id, const bool registered_actor);
 
+  // 重置方法
   void Reset();
 };
-
 } // namespace traffic_manager
 } // namespace carla
