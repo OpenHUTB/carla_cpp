c997004f4c2f0205df96a6267ef5d62a905b7c65
Muyunshui <3030155157@qq.com>
2024-10-19 17:21:29 +0800

    对Carla中交叉路口解析器的部分函数注释 (#804)
    * tianjiazhushi
    
    * zhushi
    
    * 添加注释
    
    * 对CarlaEngine的部分函数进行了注释
    
    * 对于解析OpenDRIVE格式的地图数据，并将其构建到CARLA模拟器的地图中的注释
    
    * 对于解析OpenDRIVE格式的地图数据，并将其构建到CARLA模拟器的地图中的注释
    
    * 对于Carla中配置交通管理参数的函数的注释
    
    * 对于Carla中配置交通管理参数的函数的注释(2)
    
    * 对于Carla中配置交通管理参数的函数的注释(3)
    
    * 对于Carla中配置交通管理参数的函数的注释(4)
    
    * 对于Carla中配置交通管理参数的函数的注释(5)
    
    * 对于Carla中配置交通管理参数的函数的注释(6)
    
    * 对Carla中几何解析器的部分函数注释
    
    * 对Carla中地理参考解析器的部分函数注释
    
    * 对Carla中地理参考解析器的部分函数注释(改)
    
    * 对Carla中交叉路口解析器的部分函数注释
    
    * 对Carla中车道解析器的部分函数注释
    
    * 对Carla中物体解析器的部分函数注释
    
    * 对Carla中道路配置文件解析器的部分函数注释
    
    * 对Carla中道路信息解析器的部分函数注释

--- LibCarla/source/carla/opendrive/parser/LaneParser.h
+++ LibCarla/source/carla/opendrive/parser/LaneParser.h
@@ -5,23 +5,30 @@
 // For a copy, see <https://opensource.org/licenses/MIT>.
 
 #pragma once
-
+/// @brief 提供XML文档解析功能的命名空间
 namespace pugi {
+    /// @brief 表示一个XML文档的类，用于存储和解析XML数据
   class xml_document;
 } // namespace pugi
-
+/// @brief Carla自动驾驶仿真框架的命名空间
 namespace carla {
-
+    /// @brief 包含道路建模和地图构建相关功能的命名空间
 namespace road {
+    /// @brief 用于构建道路地图的类，提供地图构建的接口和方法
   class MapBuilder;
 } // namespace road
-
+/// @brief 包含OpenDrive格式道路数据解析功能的命名空间
 namespace opendrive {
+    /// @brief 包含用于解析OpenDrive文件的解析器类的命名空间
 namespace parser {
-
+    /// @brief 用于解析OpenDrive文件中的车道（Lane）信息的解析器类
   class LaneParser {
   public:
-
+    /// @brief 解析XML文档中的车道信息，并构建道路地图中的车道部分
+    /// 该函数读取XML文档中的车道数据，如车道宽度、车道类型、车道方向等 
+    /// 并使用这些数据来构建或更新道路地图中的车道部分  
+    /// @param xml [const pugi::xml_document &] 要解析的XML文档对象
+    /// @param map_builder [carla::road::MapBuilder &] 用于构建道路地图的构建器对象
     static void Parse(
         const pugi::xml_document &xml,
         carla::road::MapBuilder &map_builder);
