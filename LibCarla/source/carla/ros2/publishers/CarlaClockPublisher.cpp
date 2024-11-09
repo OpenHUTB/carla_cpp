e36a1e40ee68431bcdee598df360bc69ee1390ea
Muyunshui <3030155157@qq.com>
2024-11-09 10:29:13 +0800

    这部分代码定义了一个名为CarlaClockPublisher的类，它专门用于在CARLA环境中，通过ROS2（Robot Operating System 2）和Fast-DDS（一个数据分发服务中间件）发布时钟信息

--- LibCarla/source/carla/ros2/publishers/CarlaClockPublisher.cpp
+++ LibCarla/source/carla/ros2/publishers/CarlaClockPublisher.cpp
@@ -1,27 +1,28 @@
 #define _GLIBCXX_USE_CXX11_ABI 0
+/// @file CarlaClockPublisher.cpp
+/// @brief CarlaClockPublisher 类的实现文件，负责发布CARLA的时钟信息到ROS2系统。
+#include "CarlaClockPublisher.h"/// @brief 包含 CarlaClockPublisher 类的声明。
 
-#include "CarlaClockPublisher.h"
+#include <string>/// @brief 包含标准字符串库，用于处理字符串数据。
+// CARLA ROS2 类型支持
+#include "carla/ros2/types/ClockPubSubTypes.h"/// @brief 包含 CARLA ROS2 时钟消息的类型支持。
+#include "carla/ros2/listeners/CarlaListener.h"/// @brief 包含 CARLA ROS2 监听器的基类声明。
+// Fast-DDS 相关头文件
+#include <fastdds/dds/domain/DomainParticipant.hpp>/// @brief 包含 Fast-DDS 域参与者的类声明，用于创建、删除和管理域中的其他实体。
+#include <fastdds/dds/publisher/Publisher.hpp>/// @brief 包含 Fast-DDS 发布者的类声明，用于将数据写入到指定的主题。
+#include <fastdds/dds/topic/Topic.hpp>/// @brief 包含 Fast-DDS 主题的类声明，用于定义数据的类型和名称。
+#include <fastdds/dds/publisher/DataWriter.hpp>/// @brief 包含 Fast-DDS 数据写入器的类声明，用于将数据写入到主题。
+#include <fastdds/dds/topic/TypeSupport.hpp>/// @brief 包含 Fast-DDS 类型支持的类声明，用于为特定类型提供序列化和反序列化功能。
 
-#include <string>
-
-#include "carla/ros2/types/ClockPubSubTypes.h"
-#include "carla/ros2/listeners/CarlaListener.h"
-
-#include <fastdds/dds/domain/DomainParticipant.hpp>
-#include <fastdds/dds/publisher/Publisher.hpp>
-#include <fastdds/dds/topic/Topic.hpp>
-#include <fastdds/dds/publisher/DataWriter.hpp>
-#include <fastdds/dds/topic/TypeSupport.hpp>
-
-#include <fastdds/dds/domain/qos/DomainParticipantQos.hpp>
-#include <fastdds/dds/domain/DomainParticipantFactory.hpp>
-#include <fastdds/dds/publisher/qos/PublisherQos.hpp>
-#include <fastdds/dds/topic/qos/TopicQos.hpp>
-
-#include <fastrtps/attributes/ParticipantAttributes.h>
-#include <fastrtps/qos/QosPolicies.h>
-#include <fastdds/dds/publisher/qos/DataWriterQos.hpp>
-#include <fastdds/dds/publisher/DataWriterListener.hpp>
+#include <fastdds/dds/domain/qos/DomainParticipantQos.hpp>/// @brief 包含 Fast-DDS 域参与者QoS策略的类声明。
+#include <fastdds/dds/domain/DomainParticipantFactory.hpp>/// @brief 包含 Fast-DDS 域参与者工厂的类声明，用于创建域参与者。
+#include <fastdds/dds/publisher/qos/PublisherQos.hpp>/// @brief 包含 Fast-DDS 发布者QoS策略的类声明。
+#include <fastdds/dds/topic/qos/TopicQos.hpp> /// @brief 包含 Fast-DDS 主题QoS策略的类声明。
+// Fast-RTPS（实时发布订阅）相关头文件
+#include <fastrtps/attributes/ParticipantAttributes.h>/// @brief 包含 Fast-RTPS 参与者属性的类声明，用于配置参与者的网络属性。
+#include <fastrtps/qos/QosPolicies.h>/// @brief 包含 Fast-RTPS QoS策略的类声明，用于配置发布者和订阅者的服务质量。
+#include <fastdds/dds/publisher/qos/DataWriterQos.hpp>/// @brief 包含 Fast-DDS 数据写入器QoS策略的类声明。
+#include <fastdds/dds/publisher/DataWriterListener.hpp>/// @brief 包含 Fast-DDS 数据写入器监听器的类声明，用于接收数据写入事件。
 
 
 namespace carla {
