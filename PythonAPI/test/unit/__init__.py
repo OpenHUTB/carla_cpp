7d2057cc4d1ab868b89f0c7963d0c8580336de24
zero141271 <140483467+zero141271@users.noreply.github.com>
2024-11-19 21:50:58 +0800

    Update __init__.py (#1011)
    在运行时动态地将Carla库的路径添加到系统路径中，以便可以导入Carla模块

--- PythonAPI/test/unit/__init__.py
+++ PythonAPI/test/unit/__init__.py
@@ -3,15 +3,21 @@
 #
 # This work is licensed under the terms of the MIT license.
 # For a copy, see <https://opensource.org/licenses/MIT>.
+#在运行时动态地将Carla库的路径添加到系统路径中，以便可以导入Carla模块
 
 import glob
 import os
 import sys
 
 try:
+    #将指定路径添加到sys.path列表中,查找特定模式的文件路径
     sys.path.append(glob.glob('../../carla/dist/carla-*%d.%d-%s.egg' % (
         sys.version_info.major,
         sys.version_info.minor,
         'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+    #检查当前操作系统是否为Windows
+
 except IndexError:
+#如果glob.glob没有找到任何匹配的文件路径，将抛出IndexError异常
     pass
+    #如果发生IndexError异常，不执行任何操作
