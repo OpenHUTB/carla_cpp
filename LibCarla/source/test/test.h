5851c09a3354669b27b7810b3730fb6f17d25b7a
cenrain-m <1785580877@qq.com>
2024-12-04 13:53:40 +0800

    对每个头文件进行注释 (#1109)
    * 对每个头文件进行注释
    
    添加注释
    
    * 总结了此代码进行了实现的功能

--- LibCarla/source/test/test.h
+++ LibCarla/source/test/test.h
@@ -1,24 +1,24 @@
 // Copyright (c) 2017 Computer Vision Center (CVC) at the Universitat Autonoma
 // de Barcelona (UAB).
 //
-// This work is licensed under the terms of the MIT license.
-// For a copy, see <https://opensource.org/licenses/MIT>.
+// 本作品采用MIT许可证授权。
+// 如需副本, 请访问 <https://opensource.org/licenses/MIT>.
 
-#pragma once
-
+#pragma once  // 保证头文件只被包含一次
+// 如果不是在调试模式下 (即在Release模式下)，定义日志等级为信息级别
 #ifndef NDEBUG
 #  define LIBCARLA_LOG_LEVEL LIBCARLA_LOG_LEVEL_INFO
 #endif // NDEBUG
 
-#include "test/Buffer.h"
+#include "test/Buffer.h"  // 包含自定义的Buffer头文件
 
-#include <carla/Logging.h>
-#include <carla/profiler/Profiler.h>
+#include <carla/Logging.h>  // 引入CARLA日志相关的头文件
+#include <carla/profiler/Profiler.h>  // 引入CARLA性能分析器相关的头文件
 
-#include <gtest/gtest.h>
+#include <gtest/gtest.h>   // 引入Google Test框架的头文件
 
-#include <chrono>
-#include <cstdint>
-#include <iostream>
+#include <chrono>  // 引入C++11的时间库，用于时间相关的操作
+#include <cstdint>   // 引入C++标准库中的整数类型定义，提供了如uint16_t的定义
+#include <iostream>  // 引入C++的输入输出流库，用于标准输出
 
-constexpr uint16_t TESTING_PORT = 0u;
+constexpr uint16_t TESTING_PORT = 0u;  // 定义一个常量值TESTING_PORT，类型为uint16_t，初始值为0
