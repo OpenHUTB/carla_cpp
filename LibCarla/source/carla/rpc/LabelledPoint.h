f3a386db97a1f8c3224e9ab528221a988f17e356
fangao-is-not-fangao <1284827909@qq.com>
2024-12-11 13:52:17 +0800

    Revert "update (#1140)"
    This reverts commit 0baaa5ea8e69f14150cf4491db03c14ab0437d32.

--- LibCarla/source/carla/rpc/LabelledPoint.h
+++ LibCarla/source/carla/rpc/LabelledPoint.h
@@ -4,27 +4,27 @@
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
 
-#pragma once //防止头文件多次包含
+#pragma once
 
 #include "carla/MsgPack.h"
-#include "carla/rpc/Location.h" //引入位置相关头文件
-#include "carla/rpc/ObjectLabel.h" //引入定义城市标签相关头文件
+#include "carla/rpc/Location.h"
+#include "carla/rpc/ObjectLabel.h"
 
 namespace carla {
 namespace rpc {
 
-  struct LabelledPoint { //定义一个结构体，用于表示带标签的点
-    //构造函数
+  struct LabelledPoint {
+
     LabelledPoint () {}
     LabelledPoint (Location location, CityObjectLabel label)
      : _location(location), _label(label)
      {}
 
-    Location _location; //表示点的位置，类型为Location
+    Location _location;
 
-    CityObjectLabel _label; //表示点的标签，类型为CityObjectLabel
+    CityObjectLabel _label;
 
-    MSGPACK_DEFINE_ARRAY(_location, _label); //使用MsgPack库定义该结构体的序列化格式，便于网络传输或持久化存储
+    MSGPACK_DEFINE_ARRAY(_location, _label);
 
   };
 
