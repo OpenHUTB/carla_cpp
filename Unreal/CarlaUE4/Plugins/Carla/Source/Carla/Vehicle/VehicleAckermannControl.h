3dcf96e036c8124f9ee4500de678f3403617d821
1351964418hml <1351964419@qq.com>
2024-12-19 17:04:47 +0800

    #添加中文注释

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Vehicle/VehicleAckermannControl.h
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Vehicle/VehicleAckermannControl.h
@@ -7,24 +7,24 @@
 #pragma once
 
 #include "VehicleAckermannControl.generated.h"
-
+// 定义车辆阿克曼控制结构体
 USTRUCT(BlueprintType)
 struct CARLA_API FVehicleAckermannControl
 {
   GENERATED_BODY()
-
+  // 转向角度，范围通常在 [-1, 1] 之间
   UPROPERTY(Category = "Vehicle Ackermann Control", EditAnywhere, BlueprintReadWrite)
   float Steer = 0.0f;
-
+  // 转向速度，表示方向盘转动的速率
   UPROPERTY(Category = "Vehicle Ackermann Control", EditAnywhere, BlueprintReadWrite)
   float SteerSpeed = 0.0f;
-
+  // 车辆速度，通常以米/秒为单位
   UPROPERTY(Category = "Vehicle Ackermann Control", EditAnywhere, BlueprintReadWrite)
   float Speed = 0.0f;
-
+  // 加速度，表示速度变化率，通常以米/秒的平方 为单位
   UPROPERTY(Category = "Vehicle Ackermann Control", EditAnywhere, BlueprintReadWrite)
   float Acceleration = 0.0f;
-
+  // 加加速度（加速度的变化率），通常以米/秒的三次方 为单位
   UPROPERTY(Category = "Vehicle Ackermann Control", EditAnywhere, BlueprintReadWrite)
   float Jerk = 0.0f;
 };
