60e788a80d9d4a990e057af16838a0edec9ccfc7
Zeng-Qi37 <2757282125@qq.com>
2024-12-19 20:37:45 +0800

    Update FIleTransfer.cpp

--- LibCarla/source/carla/client/FIleTransfer.cpp
+++ LibCarla/source/carla/client/FIleTransfer.cpp
@@ -17,6 +17,9 @@ namespace client {
   #endif
   //设置文件传输的基文件夹路径
   bool FileTransfer::SetFilesBaseFolder(const std::string &path) {
+    // 首先判断传入的路径字符串是否为空。如果path为空字符串（即没有实际内容，长度为0）
+    // 那就意味着传入的路径不合法或者不符合预期，无法进行有效的基础文件夹路径设置操作
+    // 所以此时函数直接返回false，表示设置失败
     if (path.empty()) return false;
 
     // 检查路径是否以斜线结尾，否则添加
