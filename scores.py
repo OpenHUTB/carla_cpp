4c7e9d07d9cf54ad112c3eb15f9d0cd67cdd56d2
dzq168 <204177276@qq.com>
2024-12-19 13:18:15 +0800

    从collections模块中导入Counter，用于计数

--- scores.py
+++ scores.py
@@ -3,7 +3,7 @@ import requests#导入request模块，用于请求
 import argparse#导入argparse模块，用于处理命令行参数
 import os#导入os模块，用于提供与操作系统交互
 
-from collections import defaultdict
+from collections import defaultdict#从collections模块中导入Counter，用于计数
 from collections import Counter
 
 import git  # 导入git模块，用于操作Git库
