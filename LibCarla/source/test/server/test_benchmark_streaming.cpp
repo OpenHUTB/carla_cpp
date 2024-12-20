55b2a4fd45212ccb913b23f443e58a6b5c8e6d6c
xinderruila <1184062363@qq.com>
2024-12-20 15:19:56 +0800

    Update test_benchmark_streaming.cpp

--- LibCarla/source/test/server/test_benchmark_streaming.cpp
+++ LibCarla/source/test/server/test_benchmark_streaming.cpp
@@ -1,8 +1,8 @@
-// Copyright (c) 2017 Computer Vision Center (CVC) at the Universitat Autonoma
-// de Barcelona (UAB).
+// 版权所有 （c） 2017 Universitat Autonoma 计算机视觉中心 （CVC）
+// 巴塞罗那 （UAB）。
 //
-// This work is licensed under the terms of the MIT license.
-// For a copy, see <https://opensource.org/licenses/MIT>.
+// 本作品根据 MIT 许可证的条款进行许可。
+// 有关副本，请参阅 <https://opensource.org/licenses/MIT>。
 
 #include "test.h"
 
