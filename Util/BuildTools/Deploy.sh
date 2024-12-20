67bfa7e8240e0b5d3eb18ad6074b1aed414392d8
zzz479 <2081776878@qq.com>
2024-12-20 16:07:12 +0800

    Update Deploy.sh

--- Util/BuildTools/Deploy.sh
+++ Util/BuildTools/Deploy.sh
@@ -6,14 +6,36 @@
 
 
 
+# 设置REPLACE_LATEST变量为false，可能表示在上传或发布新版本时不自动替换最新版本的文件。
 REPLACE_LATEST=false
+
+# 设置DOCKER_PUSH变量为false，可能表示不自动将Docker镜像推送到远程仓库。
 DOCKER_PUSH=false
+
+# 设置AWS_COPY变量为"aws s3 cp"命令，用于后续操作中将文件复制到Amazon S3存储桶。
+# 注意：这里使用的是AWS CLI的s3 cp命令，但实际上并未指定源和目标路径。
 AWS_COPY="aws s3 cp"
+
+# 设置DOCKER变量为"docker"命令，用于后续操作中与Docker进行交互。
 DOCKER="docker"
+
+# 设置UNTAR变量为"tar -xvzf"命令，用于解压以.tar.gz格式存储的文件。
+# -x表示解压，-v表示显示解压过程，-z表示处理gzip压缩的文件，-f表示指定文件名。
 UNTAR="tar -xvzf"
+
+# 设置UPLOAD_MAPS变量为true，可能表示需要将地图数据上传到某个存储位置。
 UPLOAD_MAPS=true
+
+# 设置PROFILE变量为"--profile Jenkins-CVC"，这个值可能用于指定AWS CLI使用的配置文件或角色。
+# 在使用AWS CLI与AWS服务交互时，可以通过--profile选项指定使用的配置。
 PROFILE="--profile Jenkins-CVC"
+
+# 设置ENDPOINT变量为"--endpoint-url=https://s3.us-east-005.backblazeb2.com/"，
+# 这个值可能用于指定S3兼容存储服务的端点URL。
+# 在这里，它看起来像是指向Backblaze B2 Cloud Storage的URL，而不是Amazon S3的标准端点。
 ENDPOINT="--endpoint-url=https://s3.us-east-005.backblazeb2.com/"
+
+# 设置TEST变量为false，可能表示当前不是在进行测试操作。
 TEST=false
 
 
