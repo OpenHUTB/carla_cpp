71597835070224fbe10a683deda00ed3925dc01e
susu0201 <2768503421@qq.com>
2024-12-20 14:23:46 +0800

    Update Update.bat

--- Update.bat
+++ Update.bat
@@ -17,25 +17,25 @@ rem -- Get the last version to download ----------------------------------------
 rem ============================================================================
 rem 如果内容文件夹不存在，则创建该文件夹
 if not exist "%CONTENT_FOLDER%" mkdir "%CONTENT_FOLDER%"
-
-rem 读取内容版本信息文件的最后一行
+rem 读取内容版本信息文件的最后一行并保存
 for /F "delims=" %%a in (%CONTENT_VERSIONS%) do set "lastLine=%%a"
-rem 提取内容的唯一标识
+
+rem 从最后一行提取内容的唯一标识符
 set CONTENT_ID=%lastLine:~-16,16%
-rem 构建下载链接
+
+rem 构建内容的下载链接
 set CONTENT_LINK=https://carla-assets.s3.us-east-005.backblazeb2.com/%CONTENT_ID%.tar.gz
 
-rem 检查内容标识前两位是否为"20"
+rem 检查内容标识符是否符合预期格式
 if "%CONTENT_ID:~0,2%"=="20" (
-    rem 设置下载文件的本地路径
+    rem 设置下载文件和解压后的tar文件的本地路径
     set CONTENT_FILE=%CONTENT_FOLDER%/%CONTENT_ID%.tar.gz
-    rem 设置解压后的tar文件的本地路径
     set CONTENT_FILE_TAR=%CONTENT_FOLDER%/%CONTENT_ID%.tar
-    rem 输出内容标识和下载链接
+    rem 显示内容标识符和下载链接
     echo %CONTENT_ID%
     echo %CONTENT_LINK%
 ) else (
-    rem 如果内容标识不符合预期格式，输出错误信息
+    rem 如果内容标识符不符合预期格式，显示错误信息并跳转到错误处理
     echo Error reading the latest version from ContentVersions.txt, check last line of file %CONTENT_VERSIONS%'
     goto error_download
 )
@@ -43,35 +43,30 @@ if "%CONTENT_ID:~0,2%"=="20" (
 rem ============================================================================
 rem -- Download the content ----------------------------------------------------
 rem ============================================================================
-rem 输出下载提示信息
+
+rem 显示下载提示信息
 echo Downloading "%CONTENT_LINK%"...
+
 rem 使用PowerShell下载文件
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%CONTENT_LINK%', '%CONTENT_FILE%')"
 rem 如果下载失败，跳转到错误处理
 if %errorlevel% neq 0 goto error_download
 
-rem 输出解压提示信息
+rem 显示解压提示信息
 echo Extracting content from "%CONTENT_FILE%", this can take a while...
-rem 判断7-Zip工具是否存在
+
+rem 判断7-Zip工具是否存在并使用其解压文件
 if exist "%ProgramW6432%/7-Zip/7z.exe" (
-    rem 使用7-Zip解压文件
     "%ProgramW6432%/7-Zip/7z.exe" x "%CONTENT_FILE%" -o"%CONTENT_FOLDER%" -y
-    rem 如果解压失败，跳转到错误处理
     if %errorlevel% neq 0 goto error_download
-    rem 删除下载的文件
     del %CONTENT_FILE:/=\%
-    rem 解压tar文件
     "%ProgramW6432%/7-Zip/7z.exe" x "%CONTENT_FILE_TAR%" -o"%CONTENT_FOLDER%" -y
-    rem 如果解压失败，跳转到错误处理
     if %errorlevel% neq 0 goto error_download
-    rem 删除tar文件
     del %CONTENT_FILE_TAR:/=\%
 ) else (
     rem 如果7-Zip不存在，使用PowerShell解压文件
     powershell -Command "Expand-Archive '%CONTENT_FILE%' -DestinationPath '%CONTENT_FOLDER%'"
-    rem 如果解压失败，跳转到错误处理
     if %errorlevel% neq 0 goto error_download
-    rem 删除下载的文件
     del %CONTENT_FILE%
 )
 
