caac7bb1f9fb56f1783ec1a66170d1e1c207974c
xinderruila <1184062363@qq.com>
2024-12-20 09:44:42 +0800

    Update pugiconfig.hpp

--- LibCarla/source/third-party/pugixml/pugiconfig.hpp
+++ LibCarla/source/third-party/pugixml/pugiconfig.hpp
@@ -40,7 +40,7 @@
 // #define PUGIXML_MEMORY_OUTPUT_STACK 10240
 // #define PUGIXML_MEMORY_XPATH_PAGE_SIZE 4096
 
-// Uncomment this to switch to header-only version
+// 取消注释此项以切换到仅标头版本
 // #define PUGIXML_HEADER_ONLY
 
 // Uncomment this to enable long long support
