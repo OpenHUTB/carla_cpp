58a658370fe2bc10f93250189f96a84518548d47
zzz479 <2081776878@qq.com>
2024-12-20 15:37:38 +0800

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -6,6 +6,6 @@ git checkout main
 
 :: 将%TEMP%文件夹中的html文件拷贝到当前目录中
 
-git add .
-git commit -m update
-git push
+git add .:: 这个命令用于将当前目录（. 表示当前目录）及其子目录中的所有更改添加到 Git 的暂存区
+git commit -m update:: 这个命令用于将暂存区中的更改提交到本地 Git 仓库
+git push:: 这个命令用于将本地仓库中的提交推送到远程仓库
