d846e3ccd34a776d55cd4951fb37b10b7d7f9448
echolis <18173850595@163.com>
2024-12-21 22:54:56 +0800

    添加注释 (#2623)
    * Update Constants.h
    
    * Update InMemoryMap.cpp
    
    * Update MotionPlanStage.cpp
    
    * Update MotionPlanStage.cpp
    
    * Update MotionPlanStage.h
    
    * Update TrafficManagerLocal.cpp
    
    * Update disable-ue4-macros.h
    
    * Update Deserializer.h
    
    * Update SensorRegistry.h
    
    * Update SemanticLidarData.h
    
    * Update CarlaEpisode.h
    
    * Update CarlaEpisode.h
    
    * Update Tagger.cpp
    
    * Update CarlaServerResponse.h

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Server/CarlaServerResponse.h
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Server/CarlaServerResponse.h
@@ -5,24 +5,24 @@
 // For a copy, see <https://opensource.org/licenses/MIT>.
 
 #pragma once
-
+// 定义一个枚举类型 ECarlaServerResponse，表示 Carla 服务器的响应状态
 enum class ECarlaServerResponse
 {
-  Success,
-  ActorNotFound,
-  ComponentNotFound,
-  ActorTypeMismatch,
-  FunctionNotSupported,
-  NullActor,
-  MissingActor,
-  NotAVehicle,
-  WalkerDead,
-  NotAWalker,
-  WalkerIncompatibleController,
-  AutoPilotNotSupported,
-  CarSimPluginNotEnabled,
-  NotATrafficLight,
-  FunctionNotAvailiableWhenDormant
+  Success,// 操作成功
+  ActorNotFound, // 找不到指定的 actor
+  ComponentNotFound,// 找不到指定的组件
+  ActorTypeMismatch,// actor 类型不匹配
+  FunctionNotSupported,// 不支持的函数调用
+  NullActor,// 空的 actor
+  MissingActor,// 缺少 actor
+  NotAVehicle,// 不是车辆类型的 actor
+  WalkerDead,// 行人已死亡
+  NotAWalker,// 不是行人类型的 actor
+  WalkerIncompatibleController,// 行人控制器不兼容
+  AutoPilotNotSupported,// 自动驾驶不支持
+  CarSimPluginNotEnabled, // 车辆模拟插件未启用
+  NotATrafficLight,// 不是交通信号灯
+  FunctionNotAvailiableWhenDormant// 当处于休眠状态时函数不可用
 };
 
 FString CarlaGetStringError(ECarlaServerResponse Response);
