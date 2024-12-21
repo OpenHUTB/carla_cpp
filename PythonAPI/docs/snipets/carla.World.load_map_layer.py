2d50f31b29bf05690519627f5ad23364090a3947
juanexuan <2438147756@qq.com>
2024-12-21 19:24:57 +0800

    Update carla.World.load_map_layer.py

--- PythonAPI/docs/snipets/carla.World.load_map_layer.py
+++ PythonAPI/docs/snipets/carla.World.load_map_layer.py
@@ -1,13 +1,13 @@
-# This recipe toggles on several layers in our "_Opt" maps
+# 这个脚本示例用于在我们名为“_Opt”的地图中切换开启多个图层。
 
-# Load town one with only minimum layout (roads, sidewalks, traffic lights and traffic signs)
+# 加载城镇1（Town01_Opt）地图，并且只加载最基础的布局元素（包括道路、人行道、交通信号灯以及交通标识）
 world = client.load_world('Town01_Opt', carla.MapLayer.None)
 
-# Toggle all buildings on
+# 切换开启所有建筑物图层
 world.load_map_layer(carla.MapLayer.Buildings)
 
-# Toggle all foliage on
+# 切换开启所有植被（ foliage ）图层
 world.load_map_layer(carla.MapLayer.Foliage)
 
-# Toggle all parked vehicles on
+# 切换开启所有停放车辆图层
 world.load_map_layer(carla.MapLayer.ParkedVehicles)
