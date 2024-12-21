c761d4fc0987ab9bb1dd9538011b5a9536e1d0a5
xuweier <2558424868@qq.com>
2024-12-21 18:43:54 +0800

    注释#Update ActorBlueprintFunctionLibrary.h (#2509)

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Actor/ActorBlueprintFunctionLibrary.h
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Actor/ActorBlueprintFunctionLibrary.h
@@ -28,9 +28,10 @@ class AShaderBasedSensor;
 struct FLidarDescription;
 
 UCLASS()
-class UActorBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+class UActorBlueprintFunctionLibrary : public UBlueprintFunctionLibrary// 继承自UBlueprintFunctionLibrary。
 {
   GENERATED_BODY()
+// Unreal Engine的宏，用于自动生成类的某些部分。
 
 public:
 
@@ -54,111 +55,112 @@ public:
   /// @name 创建角色定义的辅助工具
   /// ==========================================================================
   /// @{
-
+ /// 创建一个通用的参与者定义。
   static FActorDefinition MakeGenericDefinition(
       const FString &Category,
       const FString &Type,
       const FString &Id);
-
+/// 创建一个通用的传感器参与者定义。
   static FActorDefinition MakeGenericSensorDefinition(
       const FString &Type,
       const FString &Id);
-
+/// 创建一个相机参与者定义。
   static FActorDefinition MakeCameraDefinition(
       const FString &Id,
       bool bEnableModifyingPostProcessEffects = false);
 
+/// 创建一个相机参与者定义，并返回操作是否成功。
   UFUNCTION(Category = "Carla Actor", BlueprintCallable)
   static void MakeCameraDefinition(
       const FString &Id,
       bool bEnableModifyingPostProcessEffects,
       bool &Success,
       FActorDefinition &Definition);
-
+/// 创建一个法线相机参与者定义。
   static FActorDefinition MakeNormalsCameraDefinition();
-
+/// 创建一个法线相机参与者定义，并返回操作是否成功。
   UFUNCTION(Category = "Carla Actor", BlueprintCallable)
   static void MakeNormalsCameraDefinition(
       bool &Success,
       FActorDefinition &Definition);
-
+/// 创建一个激光雷达参与者定义。
   static FActorDefinition MakeLidarDefinition(
       const FString &Id);
-
+/// 创建一个激光雷达参与者定义，并返回操作是否成功。
   UFUNCTION(Category = "Carla Actor", BlueprintCallable)
   static void MakeLidarDefinition(
       const FString &Id,
       bool &Success,
       FActorDefinition &Definition);
-
+/// 创建一个全球导航卫星系统传感器参与者定义。
   static FActorDefinition MakeGnssDefinition();
-
+/// 创建一个全球导航卫星系统传感器参与者定义，并返回操作是否成功。
   UFUNCTION(Category = "Carla Actor", BlueprintCallable)
   static void MakeGnssDefinition(
       bool &Success,
       FActorDefinition &Definition);
-
+/// 创建一个惯性测量单元参与者定义。
   static FActorDefinition MakeIMUDefinition();
-
+/// 创建一个惯性测量单元参与者定义，并返回操作是否成功。
   UFUNCTION(Category = "Carla Actor", BlueprintCallable)
   static void MakeIMUDefinition(
       bool &Success,
       FActorDefinition &Definition);
-
+/// 创建一个雷达参与者定义。
   static FActorDefinition MakeRadarDefinition();
-
+/// 创建一个雷达参与者定义，并返回操作是否成功。
   UFUNCTION(Category = "Carla Actor", BlueprintCallable)
   static void MakeRadarDefinition(
       bool &Success,
       FActorDefinition &Definition);
-
+/// 创建一个车对外界信息交换传感器参与者定义。
   static FActorDefinition MakeV2XDefinition();
-
+/// 创建一个车对外界信息交换传感器参与者定义，并返回操作是否成功。
   UFUNCTION(Category = "Carla Actor", BlueprintCallable)
   static void MakeV2XDefinition(
       bool &Success,
       FActorDefinition &Definition);
-
+/// 创建一个自定义车对外界信息交换传感器参与者定义。
   static FActorDefinition MakeCustomV2XDefinition();
-
+/// 创建一个自定义车对外界信息交换传感器参与者定义，并返回操作是否成功。
   UFUNCTION(Category = "Carla Actor", BlueprintCallable)
   static void MakeCustomV2XDefinition(
       bool &Success,
       FActorDefinition &Definition);            
-
+/// 根据车辆参数创建车辆参与者定义，并返回操作是否成功。
   UFUNCTION(Category = "Carla Actor", BlueprintCallable)
   static void MakeVehicleDefinition(
       const FVehicleParameters &Parameters,
       bool &Success,
       FActorDefinition &Definition);
-
+/// 根据车辆参数数组创建多个车辆参与者定义。
   UFUNCTION(Category = "Carla Actor", BlueprintCallable)
   static void MakeVehicleDefinitions(
       const TArray<FVehicleParameters> &ParameterArray,
       TArray<FActorDefinition> &Definitions);
-
+/// 根据行人参数创建行人参与者定义，并返回操作是否成功。
   UFUNCTION(Category = "Carla Actor", BlueprintCallable)
   static void MakePedestrianDefinition(
       const FPedestrianParameters &Parameters,
       bool &Success,
       FActorDefinition &Definition);
-
+/// 根据行人参数数组创建多个行人参与者定义。
   UFUNCTION(Category = "Carla Actor", BlueprintCallable)
   static void MakePedestrianDefinitions(
       const TArray<FPedestrianParameters> &ParameterArray,
       TArray<FActorDefinition> &Definitions);
-
+ /// 根据ID数组创建触发器参与者定义。
   UFUNCTION(Category = "Carla Actor", BlueprintCallable)
   static void MakeTriggerDefinitions(
       const TArray<FString> &ParameterArray,
       TArray<FActorDefinition> &Definitions);
-
+/// 根据ID创建触发器参与者定义，并返回操作是否成功。
   UFUNCTION(Category = "Carla Actor", BlueprintCallable)
   static void MakeTriggerDefinition(
       const FString &Id,
       bool &Success,
       FActorDefinition &Definition);
-
+/// 根据道具参数创建道具参与者定义，并返回操作是否成功。
   UFUNCTION(Category = "Carla Actor", BlueprintCallable)
   static void MakePropDefinition(
       const FPropParameters &Parameters,
@@ -181,28 +183,28 @@ public:
   /// @name 获取属性值的帮助程序
   /// ==========================================================================
   /// @{
-
+  /// 将参与者属性转换为布尔值，如果不存在则返回默认值。
   UFUNCTION(Category = "Carla Actor", BlueprintCallable)
   static bool ActorAttributeToBool(const FActorAttribute &ActorAttribute, bool Default);
-
+/// 将参与者属性转换为整数，如果不存在则返回默认值。
   UFUNCTION(Category = "Carla Actor", BlueprintCallable)
   static int32 ActorAttributeToInt(const FActorAttribute &ActorAttribute, int32 Default);
-
+/// 将参与者属性转换为浮点数，如果不存在则返回默认值。
   UFUNCTION(Category = "Carla Actor", BlueprintCallable)
   static float ActorAttributeToFloat(const FActorAttribute &ActorAttribute, float Default);
-
+/// 将参与者属性转换为字符串，如果不存在则返回默认值。
   UFUNCTION(Category = "Carla Actor", BlueprintCallable)
   static FString ActorAttributeToString(const FActorAttribute &ActorAttribute, const FString &Default);
-
+/// 将参与者属性转换为颜色，如果不存在则返回默认值。
   UFUNCTION(Category = "Carla Actor", BlueprintCallable)
   static FColor ActorAttributeToColor(const FActorAttribute &ActorAttribute, const FColor &Default);
-
+/// 从参与者属性映射中检索布尔值，如果不存在则返回默认值。
   UFUNCTION(Category = "Carla Actor", BlueprintCallable)
   static bool RetrieveActorAttributeToBool(
       const FString &Id,
       const TMap<FString, FActorAttribute> &Attributes,
       bool Default);
-
+/// 从参与者属性映射中检索整数，如果不存在则返回默认值。
   UFUNCTION(Category = "Carla Actor", BlueprintCallable)
   static int32 RetrieveActorAttributeToInt(
       const FString &Id,
