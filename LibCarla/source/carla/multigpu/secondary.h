74a9d9f361573f30b011b13a19c77a4a03d40c3e
28996615201 <2899661520@qq.com>
2024-12-22 21:58:36 +0800

    Update secondary.h

--- LibCarla/source/carla/multigpu/secondary.h
+++ LibCarla/source/carla/multigpu/secondary.h
@@ -16,7 +16,7 @@
 #include "carla/streaming/detail/Types.h"  // 引入流相关类型的头文件
 #include "carla/ThreadPool.h"  // 引入线程池的头文件
 
-#include <boost/asio/deadline_timer.hpp>  // 引入 Boost ASIO 的定时器头文件
+#include <boost/asio/deadline_timer.hpp>  // 引入Boost ASIO库中用于设置基于绝对或相对时间点的定时器的头文件
 #include <boost/asio/io_context.hpp>  // 引入 Boost ASIO 的 IO 上下文头文件
 #include <boost/asio/ip/tcp.hpp>  // 引入 Boost ASIO 的 TCP 头文件
 #include <boost/asio/strand.hpp>  // 引入 Boost ASIO 的 Strand 头文件
