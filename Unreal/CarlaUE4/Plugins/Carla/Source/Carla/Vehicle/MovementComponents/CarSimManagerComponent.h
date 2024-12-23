2ef63e6f801bbe67220738f051ada93a2c264c3d
Huxiaoyi-1 <2339827844@qq.com>
2024-12-23 15:12:26 +0800

    对文件代码注释

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Vehicle/MovementComponents/CarSimManagerComponent.h
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Vehicle/MovementComponents/CarSimManagerComponent.h
@@ -7,76 +7,88 @@
 
 #pragma once
 
-#include "BaseCarlaMovementComponent.h"
-#include "Carla/Vehicle/VehicleControl.h"
+// 引入必要的头文件
+#include "BaseCarlaMovementComponent.h" // 基础移动组件的定义
+#include "Carla/Vehicle/VehicleControl.h" // 车辆控制相关定义
 
 #ifdef WITH_CARSIM
-#include "CarSimMovementComponent.h"
+#include "CarSimMovementComponent.h" // 只有启用 CarSim 时才包含的头文件
 #endif
 
 #include "CarSimManagerComponent.generated.h"
 
+// 前向声明 ACarlaWheeledVehicle 类
 class ACarlaWheeledVehicle;
 
-UCLASS(Blueprintable, meta=(BlueprintSpawnableComponent) )
+// UCarSimManagerComponent 类声明
+// 该类继承自 UBaseCarlaMovementComponent，表示 CarSim 管理组件
+UCLASS(Blueprintable, meta=(BlueprintSpawnableComponent))
 class CARLA_API UCarSimManagerComponent : public UBaseCarlaMovementComponent
 {
   GENERATED_BODY()
 
   #ifdef WITH_CARSIM
-  AActor* OffsetActor;
-
-  UCarSimMovementComponent * CarSimMovementComponent;
+  AActor* OffsetActor; // 用于存储偏移实体
+  UCarSimMovementComponent* CarSimMovementComponent; // CarSim 的移动组件指针
   #endif
 
 public:
 
+  // 静态方法，用于为指定车辆创建 CarSim 组件
   static void CreateCarsimComponent(
       ACarlaWheeledVehicle* Vehicle, FString Simfile);
 
-  FString SimfilePath = "";
+  FString SimfilePath = ""; // 存储 Simfile 的路径
 
+  // 在游戏开始时调用
   virtual void BeginPlay() override;
 
+  // 每帧调用，用于更新组件状态
   virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+  // 处理车辆控制输入
   void ProcessControl(FVehicleControl &Control) override;
 
+  // 获取当前速度
   FVector GetVelocity() const override;
 
+  // 在游戏结束时调用
   virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
+  // 开启或关闭 CarSim 道路模式
   void UseCarSimRoad(bool bEnabled);
 
+  // 获取当前车辆的挡位
   int32 GetVehicleCurrentGear() const override;
 
+  // 获取车辆当前的前向速度
   float GetVehicleForwardSpeed() const override;
 
+  // 禁用 CarSim 物理模拟
   void DisableCarSimPhysics();
 
+  // 禁用特殊物理模拟（覆盖基类方法）
   virtual void DisableSpecialPhysics() override;
 
 private:
 
-  // On car mesh hit, only works when carsim is enabled
+  // 当车辆与其他物体发生碰撞时调用，仅在启用 CarSim 时生效
   UFUNCTION()
-  void OnCarSimHit(AActor *Actor,
-      AActor *OtherActor,
+  void OnCarSimHit(AActor* Actor,
+      AActor* OtherActor,
       FVector NormalImpulse,
-      const FHitResult &Hit);
+      const FHitResult& Hit);
 
-  // On car mesh overlap, only works when carsim is enabled
-  // (this event triggers when overlapping with static environment)
+  // 当车辆与静态环境发生重叠时调用，仅在启用 CarSim 时生效
   UFUNCTION()
   void OnCarSimOverlap(UPrimitiveComponent* OverlappedComponent,
       AActor* OtherActor,
       UPrimitiveComponent* OtherComp,
       int32 OtherBodyIndex,
       bool bFromSweep,
-      const FHitResult & SweepResult);
+      const FHitResult& SweepResult);
 
-  // On car mesh overlap end, only works when carsim is enabled
-  // (this event triggers when overlapping with static environment)
+  // 当车辆与静态环境的重叠结束时调用，仅在启用 CarSim 时生效
   UFUNCTION()
   void OnCarSimEndOverlap(UPrimitiveComponent* OverlappedComponent,
       AActor* OtherActor,
