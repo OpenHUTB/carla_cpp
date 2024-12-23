81320af440b265c212ba69ec0d268b2ea75b64d0
jun-72 <726945401@qq.com>
2024-12-23 21:33:27 +0800

    Update VehicleVelocityControl.cpp

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Vehicle/VehicleVelocityControl.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Vehicle/VehicleVelocityControl.cpp
@@ -8,31 +8,42 @@
 
 #include "VehicleVelocityControl.h"
 
-
+// 构造函数，初始化组件的属性
 UVehicleVelocityControl::UVehicleVelocityControl()
 {
+  // 允许该组件在每帧更新时进行计算
   PrimaryComponentTick.bCanEverTick = true;
 }
-
+// 初始化时调用，设置初始状态
 void UVehicleVelocityControl::BeginPlay()
 {
-  Super::BeginPlay();
-
+  Super::BeginPlay(););  // 调用基类的 BeginPlay 方法
+  
+  // 初始时禁用组件的 Tick，手动控制启用
   SetComponentTickEnabled(false);
+  
+  // 设置 Tick 的执行时机为物理更新之前
   SetTickGroup(ETickingGroup::TG_PrePhysics);
 
+  // 获取当前拥有该组件的车辆（Actor）
   OwnerVehicle = GetOwner();
+   // 获取车辆的根组件，并强制转换为 UPrimitiveComponent 以便操作物理特性
   PrimitiveComponent = Cast<UPrimitiveComponent>(OwnerVehicle->GetRootComponent());
 }
 
+// 激活组件并设置目标速度为零向量
 void UVehicleVelocityControl::Activate(bool bReset)
 {
-  Super::Activate(bReset);
+  Super::Activate(bReset);// 调用基类的 Activate 方法
 
+  // 设置目标速度为零
   TargetVelocity = FVector();
+  
+  // 启用该组件的 Tick
   SetComponentTickEnabled(true);
 }
 
+// 激活组件并设置目标速度为指定的速度
 void UVehicleVelocityControl::Activate(FVector Velocity, bool bReset)
 {
   Super::Activate(bReset);
@@ -52,5 +63,6 @@ void UVehicleVelocityControl::TickComponent(float DeltaTime, enum ELevelTick Tic
   TRACE_CPUPROFILER_EVENT_SCOPE(UVehicleVelocityControl::TickComponent);
   FTransform Transf = OwnerVehicle->GetActorTransform();
   const FVector LocVel = Transf.TransformVector(TargetVelocity);
+  // 设置车辆根组件的线性速度（以世界坐标为单位）
   PrimitiveComponent->SetPhysicsLinearVelocity(LocVel, false, "None");
 }
