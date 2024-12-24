3e25032884cabb868d7ad9a6140434d866b01259
QEYY060506 <1023110328@qq.com>
2024-12-24 16:26:14 +0800

    Update Vehicle.h

--- LibCarla/source/carla/client/Vehicle.h
+++ LibCarla/source/carla/client/Vehicle.h
@@ -6,16 +6,59 @@
 
 #pragma once
 
-#include "carla/client/Actor.h"
-#include "carla/rpc/AckermannControllerSettings.h"
-#include "carla/rpc/TrafficLightState.h"
-#include "carla/rpc/VehicleAckermannControl.h"
-#include "carla/rpc/VehicleControl.h"
-#include "carla/rpc/VehicleDoor.h"
-#include "carla/rpc/VehicleLightState.h"
-#include "carla/rpc/VehiclePhysicsControl.h"
-#include "carla/rpc/VehicleTelemetryData.h"    // 车辆遥测数据
-#include "carla/rpc/VehicleWheels.h"
+// 包含Carla客户端中Actor相关的头文件
+// Actor在Carla模拟环境里代表各种参与模拟的实体对象，例如车辆、行人、交通标志等
+// 该头文件提供了创建、操作以及获取Actor相关属性的类、接口等相关定义
+#include "carla/client/Actor.h" 
+
+// 包含用于定义阿克曼转向控制器（Ackermann Controller）相关设置的头文件
+// 阿克曼转向常用于车辆等的转向控制模型，此头文件中可能包含如转向角度限制、控制增益等相关设置的定义
+// 方便配置相应的控制逻辑，以实现基于阿克曼转向原理的车辆操控
+#include "carla/rpc/AckermannControllerSettings.h" 
+
+// 定义交通信号灯状态相关的数据结构、枚举等内容的头文件
+// 例如包含了红灯、绿灯、黄灯等不同状态的表示方式
+// 也可能提供一些用于判断、转换交通灯状态的辅助函数或方法，有助于在模拟交通场景中处理与交通灯相关的交互逻辑
+#include "carla/rpc/TrafficLightState.h" 
+
+// 与基于阿克曼转向模型对车辆进行控制相关的头文件
+// 声明了发送给车辆使其按照阿克曼转向原理进行运动控制（像控制车辆的转向、速度等）的结构体、类或者相关函数接口
+// 方便在代码里实现对车辆具体的操控逻辑，基于阿克曼转向来模拟车辆行驶行为
+#include "carla/rpc/VehicleAckermannControl.h" 
+
+// 针对车辆控制方面更通用的头文件
+// 包含了如车辆的油门、刹车、转向等基础控制操作对应的结构体定义、控制命令的封装等内容
+// 通过使用此头文件里的定义，可以实现基本的车辆驾驶行为模拟，对车辆进行常规操作控制
+#include "carla/rpc/VehicleControl.h" 
+
+// 涉及车辆门相关操作和属性的定义的头文件
+// 例如定义了车门的打开、关闭状态表示，以及可能存在控制车门开关的函数接口声明等
+// 在模拟诸如乘车、下车等涉及车辆门状态改变的场景时会用到里面的相关功能
+#include "carla/rpc/VehicleDoor.h" 
+
+// 用来定义车辆灯光状态的头文件
+// 像车辆的大灯（远光、近光）、转向灯、刹车灯等不同灯光的亮灭状态、组合状态等相关的数据结构都在这里定义
+// 同时可能提供用于设置、查询车辆灯光状态的接口声明，有助于模拟车辆在不同行驶场景下的灯光使用情况
+#include "carla/rpc/VehicleLightState.h" 
+
+// 侧重于车辆物理特性控制方面的头文件
+// 包含对车辆的质量、惯性、摩擦力等物理参数进行设置和获取的相关类、结构体定义以及对应的操作函数接口声明等内容
+// 借助这些定义能够更精细地模拟车辆在物理环境中的运动和行为表现，实现更逼真的车辆物理模拟
+#include "carla/rpc/VehiclePhysicsControl.h" 
+
+// 定义了用于获取和表示车辆各种遥测数据的头文件（车辆遥测数据）
+// 例如车辆的实时速度、位置坐标、加速度、航向角等信息的结构体或类在此定义
+// 对于分析车辆的行驶状态、记录行驶轨迹以及基于车辆实时状态进行更复杂的控制决策等方面非常有用
+#include "carla/rpc/VehicleTelemetryData.h"  // 车辆遥测数据
+
+// 主要涉及车辆车轮相关内容的头文件
+// 像车轮的转速、转向角度、轮胎压力等属性的定义会在这里出现
+// 可能还有针对车轮单独控制或者获取其状态的函数接口声明，在模拟车辆复杂的运动、处理车轮相关故障等场景下能发挥作用
+#include "carla/rpc/VehicleWheels.h" 
+
+// 包含和Carla中交通管理相关的头文件
+// 可能包含用于管理交通流量、设置交通规则、协调不同车辆和交通参与者之间交互关系的类定义和函数接口声明
+// 例如可以用于设置车辆速度限制、生成交通流等功能，以构建更逼真的交通模拟场景
 #include "carla/trafficmanager/TrafficManager.h"
 
 using carla::traffic_manager::constants::Networking::TM_DEFAULT_PORT;
