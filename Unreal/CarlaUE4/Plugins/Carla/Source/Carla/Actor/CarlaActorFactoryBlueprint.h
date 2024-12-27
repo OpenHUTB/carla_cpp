13bbe3bd8e5ad2f8473ca5e6d4c4ff0230e2376a
combStaff <3285425748@qq.com>
2024-12-27 10:46:43 +0800

    Update CarlaActorFactoryBlueprint.h

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Actor/CarlaActorFactoryBlueprint.h
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Actor/CarlaActorFactoryBlueprint.h
@@ -1,8 +1,7 @@
-// Copyright (c) 2017 Computer Vision Center (CVC) at the Universitat Autonoma
-// de Barcelona (UAB).
+// 版权所有 (c) 2017 计算机视觉中心 (CVC) 在巴塞罗那自治大学 (UAB)。
 //
-// This work is licensed under the terms of the MIT license.
-// For a copy, see <https://opensource.org/licenses/MIT>.
+// 本作品根据 MIT 许可证的条款进行许可。
+// 了解更多，请访问 <https://opensource.org/licenses/MIT>。
 
 #pragma once
 
@@ -13,10 +12,9 @@
 
 #include "CarlaActorFactoryBlueprint.generated.h"
 
-/// Base class for Blueprints implementing ACarlaActorFactory interface.
+/// 实现 ACarlaActorFactory 接口的蓝图的基础类。
 ///
-/// Blueprints deriving from this class are expected to override
-/// GetDefinitions and SpawnActor functions.
+/// 从这个类派生的蓝图预期覆盖 GetDefinitions 和 SpawnActor 函数。
 UCLASS(Abstract, BlueprintType, Blueprintable)
 class CARLA_API ACarlaActorFactoryBlueprint : public ACarlaActorFactory
 {
@@ -26,6 +24,7 @@ public:
 
   TArray<FActorDefinition> GetDefinitions() final
   {
+    // 调用蓝图实现的 GenerateDefinitions 函数来获取演员定义
     return GenerateDefinitions();
   }
 
@@ -33,8 +32,11 @@ public:
       const FTransform &SpawnAtTransform,
       const FActorDescription &ActorDescription) final
   {
+    // 创建一个演员生成结果对象
     FActorSpawnResult Result;
+    // 调用蓝图实现的 SpawnActor 函数来生成演员
     SpawnActor(SpawnAtTransform, ActorDescription, Result);
+    // 返回生成结果
     return Result;
   }
 
